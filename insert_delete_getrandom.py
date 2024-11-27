63f461fb36b4eb8bf7b4730f2e123efb49437088
Adam Kadwory <adamkadwory@gmail.com>
2024-11-26 17:29:50 -0800

    insert

--- insert_delete_getrandom.py
+++ insert_delete_getrandom.py
@@ -1,3 +1,24 @@
-import pandas
+import random 
+
+
+class RandomizedSet:
+    def __init__(self):
+        self.data = []
+        self.indices = {}
+
+    def insert(self,val):
+        if val in self.indices:
+            return False
+        else:
+            self.data.append(val)
+            self.indices[val] = len(self.data) - 1 
+            return True,self.data
+
+x = RandomizedSet()
+print(x.insert(4))
+print(x.insert(2))
+print(x.insert(4))
+
+        
 
 
